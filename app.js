0644f08294072957d49c7100e9daa938924fb158
divyammishra7 <111589847+divyammishra7@users.noreply.github.com>
2023-01-16 22:04:39 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,135 @@
+// const p1 = {
+//     score: 0,
+//     button: document.querySelector('#p1Button'),
+//     display: document.querySelector('#p1Display')
+// }
+// const p2 = {
+//     score: 0,
+//     button: document.querySelector('#p2Button'),
+//     display: document.querySelector('#p2Display')
+// }
+
+// const resetButton = document.querySelector('#reset');
+// const winningScoreSelect = document.querySelector('#playto');
+// let winningScore = 3;
+// let isGameOver = false;
+
+// function updateScores(player, opponent) {
+//     if (!isGameOver) {
+//         player.score += 1;
+//         if (player.score === winningScore) {
+//             isGameOver = true;
+//             player.display.classList.add('has-text-success');
+//             opponent.display.classList.add('has-text-danger');
+//             player.button.disabled = true;
+//             opponent.button.disabled = true;
+//         }
+//         player.display.textContent = player.score;
+//     }
+// }
+
+
+// p1.button.addEventListener('click', function () {
+//     updateScores(p1, p2)
+// })
+// p2.button.addEventListener('click', function () {
+//     updateScores(p2, p1)
+// })
+
+
+// winningScoreSelect.addEventListener('change', function () {
+//     winningScore = parseInt(this.value);
+//     reset();
+// })
+
+// resetButton.addEventListener('click', reset)
+
+// function reset() {
+//     isGameOver = false;
+//     for (let p of [p1, p2]) {
+//         p.score = 0;
+//         p.display.textContent = 0;
+//         p.display.classList.remove('has-text-success', 'has-text-danger');
+//         p.button.disabled = false;
+//     }
+// }
+const player1=document.querySelector('.first');
+const player2=document.querySelector('.second');
+const score1=document.querySelector('#ok');
+const score2=document.querySelector('#ook');
+let pl1=0;
+let pl2=0;
+let winningScore=3;
+let isgameover=false;
+const win=document.querySelector('#winning');
+
+player1.addEventListener('click',()=>{
+  
+    if(isgameover===false){
+    pl1+=1;
+   
+  
+
+score1.textContent=pl1;
+if(pl1===winningScore){
+    isgameover=true;
+    score1.classList.add("winner");
+    score2.classList.add("looser");
+    player1.disabled=true;
+    player2.disabled=true;
+    
+}
+    }
+    
+
+
+
+})
+
+
+player2.addEventListener('click',()=>{
+    if(isgameover===false){
+  
+    pl2+=1;
+    if(pl2===winningScore){
+       
+        isgameover=true;
+        score2.classList.add("winner");
+    score1.classList.add("looser");
+    player1.disabled=true;
+    player2.disabled=true;
+    
+      
+        
+    }
+   
+   
+score2.textContent=pl2;
+    
+
+
+    }
+
+
+})
+const reset=()=>{
+    score1.textContent='0';
+    score2.textContent='0';
+    pl1=0;
+    pl2=0;
+    isgameover=false;
+    score1.classList.remove('winner','looser');
+    score2.classList.remove('winner','looser');
+    player1.disabled=false;
+    player2.disabled=false;
+}
+
+const resetr=document.querySelector('#reset');
+resetr.addEventListener('click',reset);
+
+win.addEventListener('change',()=>{
+    winningScore=parseInt(win.value);
+
+
+    reset();
+})
